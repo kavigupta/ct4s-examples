8121c851ac9eb8be89fba01f3345b01591272418
Kavi Gupta <kavi@globalmbox.com>
2016-05-04 03:06:22 -0700

    Some basic category theoretic concepts

---
+++ Category.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Category (
+
+    ) where
+
+import Prelude hiding (id, (.))
+
+{-
+    This is equivalent to Prelude's Category, but with a name that makes more sense.
+    This represents morphisms where the objects are types in Haskell
+-}
+class (Morphism m) where
+    id :: m a a
+    (.) :: m b c -> m a b -> m a c
+
+instance Morphism (->) where
+    id x = x
+    (f . g) x = f (g x)
+
+{-
+    Represents a class, or a generalized Set.
+-}
+class (Class c a) where
+    contains :: c a -> a -> Bool
+
+class (Class s a) => (Set s a) where
+    empty :: s a
+    cardinality :: (Integral u) => s a -> u
+    u :: s a -> s a -> s a
+    (&) :: s a -> s a -> s a
+    (\\) :: s a -> s a -> s a
+
+{-
+    Represents a category within Haskell. Objects are from a given Haskell type
+        and can be limited by the `ob` function.
+-}
+
+class (Category ob morph) where
+    ob :: (Class c ob) => c ob
+    morph :: (Set morphset morph) => ob -> ob -> morphset morph
+    idcat :: ob -> morph
+    compcat :: morph -> morph -> morph
