b2d3b1f0c7a84e86c1c8e2f8fbaa708397a6388a
Kavi Gupta <kavi@globalmbox.com>
2016-05-22 22:19:23 -0700

    Finished section on finite sets

--- src/FinCategory.v
+++ src/FinCategory.v
@@ -2,7 +2,7 @@ Require Import Coq.Sets.Ensembles.
 Require Import Coq.Sets.Finite_sets.
 
 
-Load category.
+Require Import category.
 Require Import SetCategory.
 
 Inductive Fin (U : Type) : Type
@@ -12,10 +12,58 @@ Inductive Fin (U : Type) : Type
 Definition set_of (U : Type) (x : Fin U) : Ensemble U
     := match x with finite set _ => set end.
 
-Inductive finarrow (U : Type) (X Y : Fin U) : Type
-    := create_finarrow (arrow : setarrow U (set_of U X) (set_of U Y)).
+Definition finarrow (U : Type) (X Y : Fin U) : Type
+    := setarrow U (set_of U X) (set_of U Y).
 
-Definition to_finarrow (U : Type) (X Y : Fin U) (arrow : setarrow U (set_of X) (set_of Y)) : finarrow U X Y
+Definition to_finarrow (U : Type) (X Y : Fin U) (arrow : setarrow U (set_of U X) (set_of U Y)) : finarrow U X Y
     := match arrow with
-        createarrow f proof => create_finarrow (createarrow f proof)
+        createarrow f proof => createarrow U (set_of U X) (set_of U Y) f proof
         end.
+
+Definition id_finarrow (U : Type) (X : Fin U) : finarrow U X X
+    := to_finarrow U X X (id_set U (set_of U X)).
+
+Definition comp_finarrow (U : Type) (X Y Z : Fin U) (f : finarrow U Y Z) (g : finarrow U X Y)
+        : finarrow U X Z
+        := 
+            comp_set U (set_of U X) (set_of U Y) (set_of U Z) f g.
+
+
+Instance FinCat (U : Type) : Category (id_finarrow U) (comp_finarrow U).
+Proof.
+    split.
+        intros.
+        destruct a as [a pA].
+        destruct b as [b pB].
+        destruct c as [c pC].
+        destruct d as [d pD].
+        unfold comp_finarrow.
+        simpl.
+        exact (comp_assoc a b c d x y z).
+
+        intros.
+        destruct a as [a pA].
+        destruct b as [b pB].
+        unfold comp_finarrow.
+        unfold id_finarrow.
+        unfold to_finarrow.
+        unfold set_of.
+        unfold id_set.
+        pose (id_law := id_left a b f).
+        unfold id_set in id_law.
+        rewrite id_law.
+        trivial.
+
+        intros.
+        destruct a as [a pA].
+        destruct b as [b pB].
+        unfold comp_finarrow.
+        unfold id_finarrow.
+        unfold to_finarrow.
+        unfold set_of.
+        unfold id_set.
+        pose (id_law := id_right a b f).
+        unfold id_set in id_law.
+        rewrite id_law.
+        trivial.
+Qed.
