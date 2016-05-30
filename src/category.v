d5f7ca04322400326b3b271752786eb84f691f75
Kavi Gupta <kavi@globalmbox.com>
2016-05-30 15:28:57 -0700

    Reformat and refactor of some proofs

--- src/category.v
+++ src/category.v
@@ -5,16 +5,16 @@ Require Import Coq.Program.Basics.
 Class Category {O : Type} {M : O -> O -> Type}
         (id : forall {x : O}, M x x)
         (comp : forall {a b c : O}, M b c -> M a b -> M a c)
-            : Prop
-    := Build_Category {
-(*It has the associated laws of associativity and left and right identity.*)
+            : Prop :=
+    Build_Category {
+    (*It has the associated laws of associativity and left and right identity.*)
         comp_assoc : forall {a b c d : O} (x : M a b) (y : M b c) (z : M c d), (comp z (comp y x) =  comp (comp z y) x);
         id_left : forall {a b : O} (f : M a b), comp id f = f;
         id_right : forall {a b : O} (f : M a b), comp f id = f
     }.
 
 Instance FunCat : Category
-        (@id) (* Identity *)
+        (@id)
         (@compose).
     split.
         trivial.
