9d385600e775857ef4d8c8cf81045e3acba884f1
Kavi Gupta <kavi@globalmbox.com>
2016-06-18 16:10:43 -0700

    Refactored Set category

--- src/SetCategory.v
+++ src/SetCategory.v
@@ -17,50 +17,41 @@ Theorem set_hom_eq {U : Type} {X Y : Ensemble U} (f g : set_hom X Y) : set_hom_f
     simpl.
     intro H.
     subst f.
-    assert (pF = pG).
-        apply proof_irrelevance.
+    assert (pF = pG) by (apply proof_irrelevance).
     rewrite H.
     reflexivity.
 Qed.
 
-Definition id_set_fn {U : Type} (u : U) : U := u.
-
-Theorem id_set_works : forall {U : Type} {X : Ensemble U} (x : U), In U X x -> In U X (id_set_fn x).
-Proof.
+Definition id_set {U : Type} (X : Ensemble U) : set_hom X X.
+    refine (cons_set_hom X X (fun u => u) _).
     trivial.
-Qed.
-
-Definition id_set {U : Type} (X : Ensemble U) : set_hom X X
-    := cons_set_hom X X id_set_fn id_set_works.
-
-Theorem comp_set_works :
-    forall {U : Type}
-    {X Y Z : Ensemble U}
-    (f g : U -> U),
-    (forall y, In U Y y -> In U Z (f y))
-        -> (forall x, In U X x -> In U Y (g x))
-        -> (forall x, In U X x -> In U Z (f (g x))).
-Proof.
-    unfold In in *.
-    intuition.
-Qed.
+Defined.
 
 Definition comp_set
         {U : Type} {X Y Z: Ensemble U}
         (f : set_hom Y Z) (g : set_hom X Y)
-            : set_hom X Z
-    := match f with
+            : set_hom X Z.
+    refine (match f with
         | cons_set_hom f' pf =>
             match g with
                 | cons_set_hom g' pg =>
-                    cons_set_hom X Z (fun (x : U) => f' (g' x)) (comp_set_works f' g' pf pg)
+                    cons_set_hom X Z (fun (x : U) => f' (g' x)) _
                 end
-       end.
+       end).
+    unfold In in *.
+    intuition.
+Defined.
 
 Instance SetCat (U : Type) : Category id_set (@comp_set U).
-Proof.
-    Hint Unfold comp_set set_hom_fn id_set id_set_fn.
-    split.
+    Hint Unfold comp_set set_hom_fn id_set.
+    split;
+        try (
+            (*left/right identity*)
+            intros A B f;
+            apply set_hom_eq;
+            destruct f;
+            reflexivity
+        ).
         (*composition*)
         intros A B C D.
         intros h g f.
@@ -68,23 +59,7 @@ Proof.
         destruct f as [f pF].
         destruct g as [g pG].
         destruct h as [h pH].
-        repeat autounfold.
-        simpl.
-        trivial.
-
-        (*left identity*)
-        intros A B f.
-        apply set_hom_eq.
-        destruct f as [f pF].
-        repeat autounfold.
-        auto.
-
-        (*right identity*)
-        intros A B f.
-        apply set_hom_eq.
-        destruct f as [f pF].
-        repeat autounfold.
-        auto.
+        reflexivity.
 Qed.
 
 Definition OSetCat (U : Type) : Cat :=
