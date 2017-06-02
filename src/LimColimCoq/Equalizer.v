a29cf7196a8650d964533718362b376c46b3e4b4
Kavi Gupta <kavi@globalmbox.com>
2017-06-02 00:25:56 -0700

    Revert "Quotients and coequalizers, can't make it work"
    This reverts commit 044871148bf1c4a5aa99fc67b12699042486cfa6.

--- src/LimColimCoq/Equalizer.v
+++
@@ -1,8 +0,0 @@
-
-Require Import Quot.Quotient.
-
-Definition equalizer {A B} (f g : A -> B) : Type :=
-    {x : A |  f x = g x}.
-
-Definition coequalizer {A B} (f g : A -> B) : Type :=
-    B / (fun x y => exists x', f x' = x /\ g x' = y).
