0ddf5191fb51db4d4711e5dd1672365efd25db92
Kavi Gupta <kavi@globalmbox.com>
2017-06-02 00:29:08 -0700

    Add paths functor, etc.

--- src/Equiv/Equivalence.v
+++ src/Equiv/Equivalence.v
@@ -10,3 +10,16 @@ Class Equiv {O : Type}
 Instance EqEq {O : Type} : @Equiv O (@eq O).
     split; intros; subst; auto.
 Qed.
+
+Inductive gen_equiv {O} (rel : O -> O -> Prop) : O -> O -> Prop :=
+    | add_refl : forall a, gen_equiv rel a a
+    | original : forall a b, rel a b -> gen_equiv rel a b
+    | reversed : forall a b, gen_equiv rel b a -> gen_equiv rel a b
+    | intermediate : forall a b witness, gen_equiv rel a witness -> gen_equiv rel witness b -> gen_equiv rel a b.
+
+Theorem EquivGen {O} (rel : O -> O -> Prop) : Equiv (gen_equiv rel).
+    split; intros.
+        apply add_refl.
+        apply (reversed rel); assumption.
+        apply (intermediate rel) with y; assumption.
+Qed.
